39646f77590a77c04a1a5dea05fddb07145c8325
Samuel Batista <batistasamuel1000@gmail.com>
2026-06-02 19:37:23 -0300

    feat: config server.js

--- server.js
+++ server.js
@@ -1,17 +1,19 @@
 const express = require('express');
 const next = require('next');
+const { parse } = require('url');
 
 const dev = process.env.NODE_ENV !== 'production';
 const port = parseInt(process.env.PORT || '3000', 10);
 
-const app = next({ dev });
+const app = next({ dev, dir: __dirname });
 const handle = app.getRequestHandler();
 
 app.prepare().then(() => {
   const server = express();
 
   server.all('*', (req, res) => {
-    return handle(req, res);
+    const parsedUrl = parse(req.url, true);
+    return handle(req, res, parsedUrl);
   });
 
   server.listen(port, () => {
