8a4225b17c2c1fec73cc3af17626edc203b6c2a0
SamuelBatista7 <samuel.batista@echoesdigital.com.br>
2026-06-02 19:19:05 -0300

    feat: Configuração de projeto deploy.

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const next = require('next');
+
+const dev = process.env.NODE_ENV !== 'production';
+const port = parseInt(process.env.PORT || '3000', 10);
+
+const app = next({ dev });
+const handle = app.getRequestHandler();
+
+app.prepare().then(() => {
+  const server = express();
+
+  server.all('*', (req, res) => {
+    return handle(req, res);
+  });
+
+  server.listen(port, () => {
+    console.log(`> Server running at http://localhost:${port}`);
+  });
+});
